3eb637e0f2122cd48a1235098ecb5d709777348b
Zheng Cao <chori84@gmail.com>
2018-03-26 20:22:02 -0700

    Implement digit match and pass tests

--- lib/digit_match.rb
+++ lib/digit_match.rb
@@ -1,4 +1,12 @@
 # Returns count of digits matching in the two input non-negative integers
 def digit_match(number1, number2)
-  raise NotImplementedError
+  return 0 if number1.nil? || number2.nil? || number1 < 0 || number2 < 0
+
+  count = 0
+  length = number1 <= number2 ? (Math.log10(number1).to_i + 1) : (Math.log10(number2).to_i + 1)
+  length.times do |i|
+    count += 1 if (number1 % (10 ** (i + 1)) / (10 ** i)) == (number2 % (10 ** (i + 1)) / (10 ** i))
+  end
+  
+  return count
 end
